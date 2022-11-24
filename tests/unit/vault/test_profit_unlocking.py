17525f210518fda121acf7b46093d9b491f4344b
bufander <bufanderbufander@gmail.com>
2022-11-24 10:02:03 +0100

    feat: added lossy test with fees and buffer

--- tests/unit/vault/test_profit_unlocking.py
+++ tests/unit/vault/test_profit_unlocking.py
@@ -804,7 +804,7 @@ def test_loss_no_fees_no_refunds_with_buffer(
     assert vault.totalSupply() == amount + first_profit
     assert vault.totalAssets() == amount + first_profit
 
-    # We increase time and create a loss
+    # We increase time
     chain.pending_timestamp = chain.pending_timestamp + WEEK // 2
     chain.mine(timestamp=chain.pending_timestamp)
 
@@ -869,6 +869,136 @@ def test_loss_no_fees_no_refunds_with_buffer(
     assert asset.balanceOf(vault) == 0
 
     assert asset.balanceOf(fish) == fish_amount + first_profit - first_loss
+
+
+def test_loss_fees_no_refunds_with_buffer(
+    create_vault,
+    asset,
+    fish_amount,
+    create_lossy_strategy,
+    user_deposit,
+    fish,
+    add_strategy_to_vault,
+    add_debt_to_strategy,
+    gov,
+    airdrop_asset,
+    deploy_accountant,
+    set_fees_for_strategy,
+):
+    amount = fish_amount // 10
+    first_profit = fish_amount // 10
+    first_loss = fish_amount // 50
+
+    management_fee = 500
+    performance_fee = 0
+
+    vault = create_vault(asset)
+    accountant = deploy_accountant(vault)
+    airdrop_asset(gov, asset, gov, fish_amount)
+    strategy = create_lossy_strategy(vault)
+
+    # Deposit assets to vault and get strategy ready
+    user_deposit(fish, vault, asset, amount)
+    add_strategy_to_vault(gov, strategy, vault)
+    add_debt_to_strategy(gov, strategy, vault, amount)
+
+    # We create a virtual profit
+    asset.transfer(strategy, first_profit, sender=gov)
+    tx = vault.process_report(strategy, sender=gov)
+    event = list(tx.decode_logs(vault.StrategyReported))
+
+    assert_strategy_reported(
+        event[0], strategy.address, first_profit, 0, amount + first_profit, 0, 0
+    )
+    assert_price_per_share(vault, 1.0)
+    assert vault.balanceOf(vault) == first_profit
+
+    assert vault.totalSupply() == amount + first_profit
+    assert vault.totalAssets() == amount + first_profit
+
+    # We increase time and create a loss
+    chain.pending_timestamp = chain.pending_timestamp + WEEK // 2
+    chain.mine(timestamp=chain.pending_timestamp)
+
+    assert pytest.approx(vault.balanceOf(vault), rel=1e-3) == first_profit // 2
+    price_per_share = vault.totalAssets() / (amount + first_profit - first_profit // 2)
+    assert_price_per_share(vault, price_per_share)
+
+    assert (
+        pytest.approx(vault.totalSupply(), rel=1e-3)
+        == amount + first_profit - first_profit // 2
+    )
+    assert vault.totalAssets() == amount + first_profit
+
+    # We set fees after profit to simplify example
+    set_fees_for_strategy(
+        gov, strategy, accountant, management_fee, performance_fee, refund_ratio=0
+    )
+
+    # We create a virtual loss that doesn't change pps as its taken care by profit buffer
+    strategy.setLoss(gov, first_loss, sender=gov)
+    tx = vault.process_report(strategy, sender=gov)
+    event = list(tx.decode_logs(vault.StrategyReported))
+
+    assert event[0].total_fees > 0
+    assert (
+        pytest.approx(price_per_share, rel=1e-3)
+        == vault.price_per_share() / 10 ** vault.decimals()
+    )
+    assert vault.balanceOf(vault) < first_profit // 2
+
+    assert vault.totalAssets() == amount + first_profit - first_loss
+    assert vault.totalSupply() > amount
+    assert (
+        vault.totalSupply() < amount + first_profit / 2
+    )  # Because we have burned shares
+
+    # We increase time and update strategy debt to 0
+    chain.pending_timestamp = chain.pending_timestamp + days_to_secs(10)
+    chain.mine(timestamp=chain.pending_timestamp)
+
+    assert vault.balanceOf(vault) == 0
+
+    add_debt_to_strategy(gov, strategy, vault, 0)
+
+    assert vault.strategies(strategy).current_debt == 0
+
+    # TODO: REVIEW, should it be slightly lower?
+    # pps is slightly lower due to fees
+    assert (
+        vault.price_per_share() / 10 ** vault.decimals()
+        < (amount + first_profit - first_loss) / amount
+    )
+
+    assert vault.total_debt() == 0
+    assert vault.total_idle() == amount + first_profit - first_loss
+    assert vault.totalSupply() > amount  # Due to fees
+    assert vault.totalAssets() == amount + first_profit - first_loss
+
+    # Fish redeems shares
+    vault.redeem(vault.balanceOf(fish), fish, fish, [], sender=fish)
+
+    assert vault.total_debt() == 0
+    assert vault.totalSupply() == vault.balanceOf(accountant)
+    assert asset.balanceOf(vault) == vault.convertToAssets(vault.balanceOf(accountant))
+
+    assert asset.balanceOf(fish) < fish_amount + first_profit - first_loss
+    assert asset.balanceOf(fish) > fish_amount
+
+    # Accountant redeems shares
+    vault.redeem(
+        vault.balanceOf(accountant), accountant, accountant, [], sender=accountant
+    )
+
+    assert vault.total_debt() == 0
+    assert vault.totalSupply() == 0
+    assert vault.totalAssets() == 0
+    assert asset.balanceOf(vault) == 0
+
+    assert (
+        asset.balanceOf(accountant) + asset.balanceOf(fish)
+        == fish_amount + first_profit - first_loss
+    )
 
 
 def test_loss_no_fees_no_refunds_with_not_enough_buffer(
