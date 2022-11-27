a26f4fb290e43d3acbf9b18aed8ac6bc985ad008
jmonteer <jmonteer@protonmail.com>
2022-11-27 18:19:44 +0100

    fix: formatting

--- tests/unit/factory/test_protocol_fees_config.py
+++ tests/unit/factory/test_protocol_fees_config.py
@@ -1,9 +1,8 @@
 import ape
 from ape import chain
 
-def test__set_protocol_fees(
-    gov, vault_factory
-):
+
+def test__set_protocol_fees(gov, vault_factory):
     assert vault_factory.protocol_fee_config().fee_last_change == 0
     last_change = vault_factory.protocol_fee_config().fee_last_change
     tx = vault_factory.set_protocol_fee_bps(20, sender=gov)
@@ -13,35 +12,29 @@ def test__set_protocol_fees(
     assert event[0].new_fee_bps == 20
 
     assert vault_factory.protocol_fee_config().fee_bps == 20
-    assert vault_factory.protocol_fee_config().fee_last_change > last_change 
+    assert vault_factory.protocol_fee_config().fee_last_change > last_change
 
-def test__set_protocol_fee_recipient(
-    gov, vault_factory
-):
+
+def test__set_protocol_fee_recipient(gov, vault_factory):
     tx = vault_factory.set_protocol_fee_recipient(gov.address, sender=gov)
 
     event = list(tx.decode_logs(vault_factory.UpdateProtocolFeeRecipient))
-    assert event[0].old_fee_recipient == '0x0000000000000000000000000000000000000000'
+    assert event[0].old_fee_recipient == "0x0000000000000000000000000000000000000000"
     assert event[0].new_fee_recipient == gov.address
 
     assert vault_factory.protocol_fee_config().fee_recipient == gov.address
 
-def test__set_protocol_fee_recipient_by_bunny__reverts(
-    bunny, vault_factory
-):
+
+def test__set_protocol_fee_recipient_by_bunny__reverts(bunny, vault_factory):
     with ape.reverts("not governance"):
         vault_factory.set_protocol_fee_recipient(bunny.address, sender=bunny)
 
-def test__set_protocol_fees_too_high__reverts(
-    gov, vault_factory
-):
+
+def test__set_protocol_fees_too_high__reverts(gov, vault_factory):
     with ape.reverts("fee too high"):
         vault_factory.set_protocol_fee_bps(26, sender=gov)
 
-def test__set_protocol_fees_by_bunny__reverts(
-    bunny, vault_factory
-):
+
+def test__set_protocol_fees_by_bunny__reverts(bunny, vault_factory):
     with ape.reverts("not governance"):
         vault_factory.set_protocol_fee_bps(20, sender=bunny)
-
-
--- tests/unit/vault/test_protocol_fees.py
+++ tests/unit/vault/test_protocol_fees.py
@@ -3,6 +3,7 @@ import pytest
 from utils.constants import ROLES
 from utils.utils import days_to_secs
 
+
 def test__report_with_protocol_fees(
     vault_factory,
     create_vault,
@@ -29,12 +30,13 @@ def test__report_with_protocol_fees(
     add_strategy_to_vault(gov, strategy, vault)
     add_debt_to_strategy(gov, strategy, vault, amount)
 
-    assert vault.price_per_share() == int(10 ** 18)
+    assert vault.price_per_share() == int(10**18)
     days_passed = 365
     # We increase time after profit has been released and check estimation
     chain.pending_timestamp = vault.last_report() + days_to_secs(days_passed)
     tx = vault.process_report(strategy, sender=gov)
 
     assert vault.balanceOf(gov.address) == amount * 0.0025 * days_passed / 365
-    assert pytest.approx(vault.price_per_share(), rel=1e-5) == int(10 ** 18) * (1 - 0.0025 * days_passed / 365)
-
+    assert pytest.approx(vault.price_per_share(), rel=1e-5) == int(10**18) * (
+        1 - 0.0025 * days_passed / 365
+    )
