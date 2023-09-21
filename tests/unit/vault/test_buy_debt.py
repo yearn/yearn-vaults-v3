3c13e1e7be661e11fc7840a5259314d11dee8be9
FP <fp_crypt@protonmail.com>
2023-09-21 10:24:25 -0700

    chore: test working as expected

--- tests/unit/vault/test_buy_debt.py
+++ tests/unit/vault/test_buy_debt.py
@@ -11,7 +11,8 @@ def set_role(vault, gov):
         | ROLES.REVOKE_STRATEGY_MANAGER
         | ROLES.DEBT_MANAGER
         | ROLES.MAX_DEBT_MANAGER
-        | ROLES.DEBT_PURCHASER,
+        | ROLES.DEBT_PURCHASER
+        | ROLES.REPORTING_MANAGER,
         sender=gov,
     )
 
@@ -178,24 +179,31 @@ def test_buy_debt__cover_loss(
     strategy = lossy_strategy
 
     amount = fish_amount
-
+ 
     mint_and_deposit_into_vault(vault, gov, amount)
 
     add_debt_to_strategy(gov, strategy, vault, amount)
 
-    to_buy = amount // 2
-
     # Approve vault to pull funds.
-    asset.mint(gov.address, to_buy, sender=gov)
-    asset.approve(vault.address, to_buy, sender=gov)
+    asset.mint(gov.address, amount, sender=gov)
+    asset.approve(vault.address, amount, sender=gov)
 
     before_balance = asset.balanceOf(gov)
     before_shares = strategy.balanceOf(gov)
 
-    strategy.setLoss(gov, to_buy, sender=gov)
+    amount_to_lose = amount // 2
 
-    vault.buy_debt(strategy, to_buy, sender=gov)
+    strategy.setLoss(gov, amount_to_lose, sender=gov)
+    assert asset.balanceOf(strategy) < amount
 
+    vault.process_report(strategy.address, sender=gov)
+
+    asset.transfer(strategy, amount // 2, sender=gov)
+    assert asset.balanceOf(strategy) == amount
+
+    vault.buy_debt(strategy, amount // 2, sender=gov)
+
+    assert asset.balanceOf(strategy) == amount
     assert vault.totalIdle() == amount
     assert vault.totalDebt() == 0
     assert vault.pricePerShare() == 10 ** asset.decimals()
@@ -203,3 +211,5 @@ def test_buy_debt__cover_loss(
     # assert shares got moved
     assert asset.balanceOf(gov) == before_balance - amount
     assert strategy.balanceOf(gov) == before_shares + amount
+
+
