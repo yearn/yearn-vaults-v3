f4d3de2b5c40d6f0b49c9b2f85feffd1fd04bafe
Schlagonia <schlagonia@gmail.com>
2024-01-31 19:47:37 -0700

    chore: setup foundry test

---
+++ foundry_tests/utils/Setup.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.18;
+
+import {ExtendedTest} from "./ExtendedTest.sol";
+
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+
+contract Setup is ExtendedTest  {
+
+
+
+    function setUp() public virtual {}
+
+
+}
